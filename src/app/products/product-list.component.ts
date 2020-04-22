a46b05be4177f7bc38c89e266f26d6b7da4b0953
Duvan <wilsonduvan@gmail.com>
2020-04-21 22:35:16 -0500

    Add style on thead and ngOnInit

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,11 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IProducts} from './products';
 
 @Component({
   selector: 'app-product-list',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
   pageTitle = 'Product List!';
   imageWidth: number = 50;
   imageMargin: number = 2;
@@ -66,5 +67,9 @@ export class ProductListComponent {
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log('In OnInit');
   }
 }
