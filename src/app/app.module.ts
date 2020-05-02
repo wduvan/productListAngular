394553eb48cca42ec469839067b1b00507493970
Duvan <wilsonduvan@gmail.com>
2020-05-01 19:31:41 -0500

    Add styles and works routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-
 import {AppComponent} from './app.component';
 import {ServiceWorkerModule} from '@angular/service-worker';
 import {environment} from '../environments/environment';
-import {WelcomeComponent} from './home/home.component';
+import {WelcomeComponent} from './home/welcome.component';
 import {ProductListComponent} from './products/product-list.component';
 import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
 import {StarComponent} from './shared/star/star.component';
 import {HttpClientModule} from '@angular/common/http';
+import {ProductDetailComponent} from './products/product-detail.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -17,13 +18,21 @@ import {HttpClientModule} from '@angular/common/http';
     WelcomeComponent,
     ProductListComponent,
     StarComponent,
-    ConvertToSpacesPipe
+    ConvertToSpacesPipe,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'products', component: ProductListComponent},
+      {path: 'products/:id', component: ProductDetailComponent},
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ], {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
