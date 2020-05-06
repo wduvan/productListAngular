6a3e300830da5564f0ad0fd3afcd58230d8372d4
Duvan <wilsonduvan@gmail.com>
2020-05-06 18:09:57 -0500

    Add detail product in the router link

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {IProducts} from './products';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,10 +11,27 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProducts;
 
-  constructor() {
+  constructor(private route: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit(): void {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': id,
+      'productName': 'Leaf Rake',
+      'productCode': 'GDN-0011',
+      'releaseDate': 'March 19, 2019',
+      'description': 'Leaf rake with 48-inch wooden handle.',
+      'price': 19.95,
+      'starRating': 3.2,
+      'imageUrl': 'assets/images/leaf_rake.png'
+    };
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
